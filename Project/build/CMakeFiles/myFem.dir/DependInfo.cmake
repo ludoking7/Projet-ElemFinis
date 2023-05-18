b22455d5aa53c6ae667d33ac902f65650ca0207f
Ludovic Leroy <ludoleroy7@gmail.com>
2023-05-18 03:43:48 +0200

    Bandsolver Yg

---
+++ Project/build/CMakeFiles/myFem.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ludov/OneDrive/Documents/UCLouvain/Q6/ElemFinis/Project ElemFinis/Project/src/fem.c" "CMakeFiles/myFem.dir/src/fem.c.obj" "gcc" "CMakeFiles/myFem.dir/src/fem.c.obj.d"
+  "C:/Users/ludov/OneDrive/Documents/UCLouvain/Q6/ElemFinis/Project ElemFinis/Project/src/homework.c" "CMakeFiles/myFem.dir/src/homework.c.obj" "gcc" "CMakeFiles/myFem.dir/src/homework.c.obj.d"
+  "C:/Users/ludov/OneDrive/Documents/UCLouvain/Q6/ElemFinis/Project ElemFinis/Project/src/main.c" "CMakeFiles/myFem.dir/src/main.c.obj" "gcc" "CMakeFiles/myFem.dir/src/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
